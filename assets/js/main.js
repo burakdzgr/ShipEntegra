0e7801f37d56e34216b980c17252e8804c5a36c1
Burak Dizieğri <61769761+burakdzgr@users.noreply.github.com>
2022-06-16 22:07:49 +0300

    sidebar revize v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,38 @@
+const targetEl = document.getElementById('dropdown-example');
+
+const triggerEl = document.querySelector('.payments-btn');
+
+const options = {
+    placement: 'bottom',
+    onHide: () => {
+        console.log('dropdown has been hidden');
+    },
+    onShow: () => {
+        console.log('dropdown has been shown');
+    }
+  };
+const dropdown = new Dropdown(targetEl,null,options);
+
 $(document).on("click","#nav-toggle",function(){
     var checkClass = $("#sidebar").hasClass("mobile-sidebar");
     if ( checkClass ){
         $("#sidebar").removeClass("mobile-sidebar");
         $("#content").removeClass("content-full");
+        $(".second-list-a").addClass("pl-11");
         $(".sidebar-items-badge").show(100);
         $(".sidebar-items-text").show(100)
         $(".sidebar-items-second-icon").show(100);
         $(".sidebar-toggle-icon ").show(100);
         $("#social-list").removeClass("flex-col");
-
+        dropdown.show()
     }else {
         $("#sidebar").addClass("mobile-sidebar");
         $("#content").addClass("content-full");
         $("#social-list").addClass("flex-col");
+        $(".second-list-a").removeClass("pl-11");
         $(".sidebar-items-badge").hide(100);
         $(".sidebar-toggle-icon ").hide(100);
-        $("#dropdown-example").hide(100)
         $(".sidebar-items-text").hide(100)
+        dropdown.hide();
     }
 });

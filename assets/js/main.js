d15768c65bff4542d0b5ae3974dc45a389edab92
Burak Dizieğri <61769761+burakdzgr@users.noreply.github.com>
2022-07-04 00:54:11 +0300

    product detail page added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
+$(function () {
+
+    var rating = "(0)";
+    $(".counter").text(rating);
+    $(".product-detail-rate-star").rateYo({
+        starWidth: "20px",
+        normalFill: "#e0e0e0",
+        fullStar: true,
+    });
+    $(".product-rate-star").rateYo({
+        starWidth: "13px",
+        normalFill: "#e0e0e0",
+        fullStar: true,
+    });
+  });
 const targetEl = document.getElementById('dropdown-example');
 
 const triggerEl = document.querySelector('.payments-btn');
@@ -55,4 +70,27 @@ $(".price-range").ionRangeSlider({
            var query = buildUrl(window.location.pathname,window.filterObj);
           window.location.href = query
     }
+  });
+  var swiper = new Swiper('.product-detail-image-thumb', {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    freeMode: false,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: '.product-slide-thumb-arrow-right',
+      prevEl: '.product-slide-thumb-arrow-left',
+    }
+  });
+  
+  var swiper = new Swiper('.product-detail-image-main', {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.product-slide-arrow-right',
+        prevEl: '.product-slide-arrow-left',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    },
+    loop: false
   });
